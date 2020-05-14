96e8d2f4b10a2e71fa1c7591081acac51ad3890a
Srividhya R <54398008+srivi15@users.noreply.github.com>
2020-05-14 17:59:07 +0530

    Assignment  =, *, ** operator

---
+++ Intro to python/variable operations.py
@@ -0,0 +1,11 @@
+# Create a variable savings
+savings = 100
+
+# Create a variable growth_multiplier
+growth_multiplier = 1.1
+
+# Calculate result
+result = 100 * 1.1 ** 7
+
+# Print out result
+print(result)
