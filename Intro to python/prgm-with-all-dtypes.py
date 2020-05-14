7b0b59bd77e693a46a4ad338ba615f94756c7f34
Srividhya R <54398008+srivi15@users.noreply.github.com>
2020-05-14 18:03:04 +0530

    sample prgm with all dtypes

---
+++ Intro to python/prgm-with-all-dtypes.py
@@ -0,0 +1,17 @@
+savings = 100
+growth_multiplier = 1.1
+desc = "compound interest"
+
+# Assign product of growth_multiplier and savings to year1
+year1 = growth_multiplier * savings
+
+# Print the type of year1
+print(type(year1))
+print(year1)
+
+# Assign sum of desc and desc to doubledesc
+doubledesc = desc + desc
+d = (desc == desc)
+print(d)
+# Print out doubledesc
+print(doubledesc)
