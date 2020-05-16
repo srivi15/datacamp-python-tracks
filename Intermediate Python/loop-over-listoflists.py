84dd30cb091298f6c150fd9b93b39e93785674fa
Srividhya R <54398008+srivi15@users.noreply.github.com>
2020-05-16 17:43:53 +0530

    for loop over list of lists

---
+++ Intermediate Python/loop-over-listoflists.py
@@ -0,0 +1,10 @@
+# house list of lists
+house = [["hallway", 11.25], 
+         ["kitchen", 18.0], 
+         ["living room", 20.0], 
+         ["bedroom", 10.75], 
+         ["bathroom", 9.50]]
+         
+# Build a for loop from scratch
+for x, y in house:
+    print("the " + str(x) + " is " + str(y) + " sqm")
