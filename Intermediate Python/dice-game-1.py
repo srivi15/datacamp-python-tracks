d17f876542a48fc757c3d6838f6ec5208dc55c54
Srividhya R <54398008+srivi15@users.noreply.github.com>
2020-05-16 18:45:03 +0530

    dice game using random

---
+++ Intermediate Python/dice-game-1.py
@@ -0,0 +1,19 @@
+# Numpy is imported, seed is set
+
+# Starting step
+step = 50
+
+# Roll the dice
+dice = np.random.randint(1,7)
+
+# Finish the control construct
+if dice <= 2 :
+    step = step - 1
+elif dice>=3 and dice<=5 :
+    step = step + 1
+else :
+    step = step + np.random.randint(1,7)
+
+# Print out dice and step
+print(dice)
+print(step)
