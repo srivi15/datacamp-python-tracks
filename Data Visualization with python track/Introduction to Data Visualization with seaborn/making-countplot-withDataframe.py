0f520f8a0fab17e9b4bc9b0554ee030d3ff81d04
Srividhya R <54398008+srivi15@users.noreply.github.com>
2020-06-28 19:10:26 +0530

    part 1

---
+++ Data Visualization with python track/Introduction to Data Visualization with seaborn/making-countplot-withDataframe.py
@@ -0,0 +1,14 @@
+# Import Matplotlib, Pandas, and Seaborn
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# Create a DataFrame from csv file
+df = pd.read_csv(csv_filepath)
+
+# Create a count plot with "Spiders" on the x-axis
+sns.countplot(x="Spiders", data=df)
+
+# Display the plot
+plt.show()
---
+++ Data Visualization with python track/Introduction to Data Visualization with seaborn/making-scatterplot-withLists.py
@@ -0,0 +1,19 @@
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Change this scatter plot to have number of phones on the y-axis
+sns.scatterplot(x=gdp, y=phones)
+
+# Show plot
+plt.show()
+
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Change this scatter plot to have percent literate on the y-axis
+sns.scatterplot(x=gdp, y=percent_literate)
+
+# Show plot
+plt.show()
