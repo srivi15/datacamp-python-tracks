fec5d24c1785e1a8e2be97f169643cac09f98b65
Srividhya R <54398008+srivi15@users.noreply.github.com>
2020-06-29 17:15:56 +0530

    part 2

---
+++ Data Visualization with python track/Introduction to Data Visualization with seaborn/creatingSubplots-withRowsndcols.py
@@ -0,0 +1,26 @@
+# Change to use relplot() instead of scatterplot()
+sns.relplot(x="absences", y="G3", 
+                data=student_data ,kind='scatter')
+
+# Show plot
+plt.show()
+
+
+# Change to make subplots based on study time
+sns.relplot(x="absences", y="G3", 
+            data=student_data,
+            kind="scatter",
+            col = "study_time")
+
+# Show plot
+plt.show()
+
+
+# Change this scatter plot to arrange the plots in rows instead of columns
+sns.relplot(x="absences", y="G3", 
+            data=student_data,
+            kind="scatter", 
+            row="study_time")
+
+# Show plot
+plt.show()
