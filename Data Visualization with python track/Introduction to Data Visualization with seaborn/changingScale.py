b1ad8b2fd2d2664717451c957e3f21eb58e902ad
Srividhya R <54398008+srivi15@users.noreply.github.com>
2020-07-01 15:56:01 +0530

    part 4

---
+++ Data Visualization with python track/Introduction to Data Visualization with seaborn/changingScale.py
@@ -0,0 +1,46 @@
+#1
+# Set the context to "paper"
+sns.set_context("paper")
+
+# Create bar plot
+sns.catplot(x="Number of Siblings", y="Feels Lonely",
+            data=survey_data, kind="bar")
+
+# Show plot
+plt.show()
+
+
+#2
+# Change the context to "notebook"
+sns.set_context("notebook")
+
+# Create bar plot
+sns.catplot(x="Number of Siblings", y="Feels Lonely",
+            data=survey_data, kind="bar")
+
+# Show plot
+plt.show()
+
+
+#3
+# Change the context to "talk"
+sns.set_context("talk")
+
+# Create bar plot
+sns.catplot(x="Number of Siblings", y="Feels Lonely",
+            data=survey_data, kind="bar")
+
+# Show plot
+plt.show()
+
+
+#4
+# Change the context to "poster"
+sns.set_context("poster")
+
+# Create bar plot
+sns.catplot(x="Number of Siblings", y="Feels Lonely",
+            data=survey_data, kind="bar")
+
+# Show plot
+plt.show()
---
+++ Data Visualization with python track/Introduction to Data Visualization with seaborn/changingStyleandColor.py
@@ -0,0 +1,33 @@
+#1 and 2
+# Change the color palette to "RdBu"
+sns.set_style("whitegrid")
+sns.set_palette("Purples")
+
+# Create a count plot of survey responses
+category_order = ["Never", "Rarely", "Sometimes", 
+                  "Often", "Always"]
+
+sns.catplot(x="Parents Advice", 
+            data=survey_data, 
+            kind="count", 
+            order=category_order)
+
+# Show plot
+plt.show()
+
+#3
+# Change the color palette to "RdBu"
+sns.set_style("whitegrid")
+sns.set_palette("RdBu")
+
+# Create a count plot of survey responses
+category_order = ["Never", "Rarely", "Sometimes", 
+                  "Often", "Always"]
+
+sns.catplot(x="Parents Advice", 
+            data=survey_data, 
+            kind="count", 
+            order=category_order)
+
+# Show plot
+plt.show()
